60b4af2fe016d99ca7cb67225cd08f819a3b0e06
Michael Younkin <younkin.michael@gmail.com>
2016-06-06 01:48:08 -0400

    Download notebooks and associated notes

--- Main.java
+++ Main.java
@@ -10,7 +10,11 @@ import com.evernote.clients.ClientFactory;
 import com.evernote.clients.NoteStoreClient;
 import com.evernote.clients.UserStoreClient;
 import com.evernote.thrift.transport.TTransportException;
+import com.evernote.edam.notestore.NoteFilter;
+import com.evernote.edam.notestore.NoteList;
 import com.evernote.edam.type.Notebook;
+import com.evernote.edam.type.Note;
+import com.evernote.edam.type.NoteSortOrder;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,10 +28,21 @@ public class Main {
             UserStoreClient userStore = factory.createUserStoreClient();
             NoteStoreClient noteStore = factory.createNoteStoreClient();
 
-            System.out.println("Loading notebooks...");
+            System.out.println("Loading notebooks and notes...");
             List<Notebook> notebooks = noteStore.listNotebooks();
             for (Notebook notebook : notebooks) {
-                System.out.println(notebook.getName());
+                System.out.println("Notebook: " + notebook.getName());
+                
+                NoteFilter filter = new NoteFilter();
+                filter.setNotebookGuid(notebook.getGuid());
+                filter.setOrder(NoteSortOrder.CREATED.getValue());
+                filter.setAscending(true);
+
+                NoteList noteList = noteStore.findNotes(filter, 0, 100000);
+                List<Note> notes = noteList.getNotes();
+                for (Note note : notes) {
+                    System.out.println(" * " + note.getTitle());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
